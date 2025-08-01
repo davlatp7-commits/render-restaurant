db66bb78ed5dfb147174735b2d3c5e69a55072d3
davlatp7 <davlatp7@gmail.com>
2025-08-01 14:05:34 +0300

    Автоматические миграции при запуске

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-from app import create_app
+from app import create_app, db
+from flask_migrate import upgrade
 
 app = create_app()
+
+# Выполняем миграции автоматически при запуске
+with app.app_context():
+    upgrade()
